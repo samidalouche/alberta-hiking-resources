7334e410b534b1aa0961ddca6b959abd23743947
Sami Dalouche <sami@narrative.io>
2026-01-10 21:47:46 -0400

    Updates

---
+++ _nuxt/Bqs5urVb.js
@@ -0,0 +1,1 @@
+import{r}from"./B_MBrfMn.js";import t from"./Ct6JTvzS.js";import{X as e,b as a,o as p,s as c,x as n}from"./Br-vWXU1.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(i){const o=e();return(s,f)=>(p(),a(t,{color:"success",icon:n(o).ui.icons.tip},{default:c(()=>[r(s.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
---
+++ _nuxt/DcYejIAE.js
@@ -0,0 +1,1 @@
+import{r as t}from"./B_MBrfMn.js";import a from"./Ct6JTvzS.js";import{X as s,b as e,o as n,s as c,x as i}from"./Br-vWXU1.js";import"./BN_7HF1G.js";const d={__name:"ProseCaution",setup(p){const o=s();return(r,f)=>(n(),e(a,{color:"error",icon:i(o).ui.icons.caution},{default:c(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
